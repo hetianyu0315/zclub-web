307acc127427e19197e8c6fe5bb1f78931bbd0cf
lseyesl <lseyesl@163.com>
2022-08-02 14:06:51 +0800

    feat: add page airdrop & template
    fix: disabled invite pages wallet
    
    fix: airdrop add html to jpg
    
    fix: airdrop add logic
    
    fix: Optimize airdrop logic
    
    fix: Change solana wallet link method

---
+++ src/components/airdropHeader/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './index.less';
+import logoSvg from '@/assets/images/logo.svg';
+
+export default function IndexPage(){
+
+    return <div className={styles.header}>
+        <div className={styles.logo}>
+            <a href="/">
+                <img src={logoSvg} alt="" />
+            </a>
+        </div>
+        <div className={styles.opts}>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/airdrop/index/" className={styles.active}>ZClubNFT</a></li>
+            </ul>
+        </div>
+    </div>
+}
